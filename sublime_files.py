b9f5732fdbb0a8f833459c1e35d86bef9a7f744c
ayang <ayang23@gmail.com>
2012-10-13 00:03:34 +0800

    fix the bug in windows of cannot load other drives.

--- sublime_files.py
+++ sublime_files.py
@@ -30,7 +30,16 @@ class SublimeFilesCommand(sublime_plugin.WindowCommand):
 
     #function for showing panel for changing directories / opening files
     def open_navigator(self):
-        self.dir_files = ['[' + os.getcwdu() +']', bullet + ' Directory actions', '..' + os.sep, '~' + os.sep]
+        self.dir_files = ['[' + os.getcwdu() + ']', bullet + ' Directory actions', '..' + os.sep, '~' + os.sep]
+
+        if(sublime.platform() == 'windows'):
+            self.drives = []
+            for i in range(ord('A'), ord('Z') + 1):
+                drive = chr(i)
+                if(os.path.exists(drive + ":\\")):
+                    self.drives.append(drive + ':\\')
+            self.dir_files += self.drives
+
         for element in os.listdir(os.getcwdu()):
             fullpath = os.path.join(os.getcwdu(), element)
             if os.path.isdir(fullpath):
@@ -56,6 +65,8 @@ class SublimeFilesCommand(sublime_plugin.WindowCommand):
                 os.chdir(os.getenv(self.home))
             elif option == '..' + os.sep:
                 os.chdir(os.path.pardir)
+            elif sublime.platform() == 'windows' and option in self.drives:
+                os.chdir(option)
             elif option == bullet + ' To current view':
                 os.chdir(os.path.dirname(self.window.active_view().file_name()))
             elif option.startswith(bullet + ' To bookmark'):
